64cfd91674d1abe7252573c8ab795a5e163ac3fa
girishg4t <talekar.g@gmail.com>
2021-04-08 11:22:46 +0530

    corrected file names

---
+++ cli/cmd/uninstall.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+)
+
+var uninstallCmd = &cobra.Command{
+	Use:   "uninstall",
+	Short: "Delete the installed stack",
+	Run:   helmUninstall,
+}
+
+func init() {
+	RootCmd.AddCommand(uninstallCmd)
+}
+
+func helmUninstall(cmd *cobra.Command, args []string) {
+	//TODO: need to remove hardcoded values
+	uninstall := exec.Command("helm", "delete", "my-release")
+	fmt.Println("Deleting the Prometheus stack")
+	out, err := uninstall.CombinedOutput()
+	if err != nil {
+		fmt.Printf("could not uninstall The Observability Stack: %w \nOutput: %v", err, string(out))
+	}
+}
