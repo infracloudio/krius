347ccfa6dabd86655ddf3725e0f398f9881dbb9b
Dhru1Tanna <dhruvan.tanna1@gmail.com>
2021-05-30 17:39:27 +0530

    Reverting unintended change

--- cli/cmd/utils.go
+++ cli/cmd/utils.go
@@ -24,8 +24,8 @@ func getVarFromCmd(cmd *cobra.Command, envVar, defaultValue string) string {
 
 func addConfigureObjStoreFlags(cmd *cobra.Command) {
 	cmd.Flags().StringP("config-file", "c", "", "config file path")
-	cmd.Flags().StringP("namespace", "n", "default", "namespace in which the chart has installed")
-	cmd.Flags().StringP("release", "r", "my-release", "release name of the chart")
+	cmd.Flags().StringP("namespace", "n", "", "namespace in which the chart has installed")
+	cmd.Flags().StringP("release", "r", "", "release name of the chart")
 	cmd.Flags().StringP("type", "t", "", "type of storage")
 	cmd.Flags().StringP("bucket", "b", "", "bucket name")
 	cmd.Flags().StringP("endpoint", "e", "", "bucket's endpoint")
