257d276fa1bcf352dc2c509daf662d141cf72972
YachikaRalhan <36668209+YachikaRalhan@users.noreply.github.com>
2021-06-10 22:37:31 +0530

    Added Rule Schema for spec (#26)
    * Added Rule Schema for spec
    Aded code to check user passed config file against that schema, show results
    
    * Introduced type & spec in schema validator
    
    * followed camel case naming covention & set additional properties to false (no extra KVs)

--- go.mod
+++ go.mod
@@ -6,10 +6,11 @@ require (
 	github.com/gofrs/flock v0.8.0
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.1.3
-	google.golang.org/protobuf v1.25.0
+	github.com/xeipuuv/gojsonschema v1.2.0
 	gopkg.in/yaml.v2 v2.4.0
 	helm.sh/helm/v3 v3.5.1
 	rsc.io/letsencrypt v0.0.3 // indirect
+	sigs.k8s.io/yaml v1.2.0
 )
 
 replace github.com/docker/docker => github.com/moby/moby v0.7.3-0.20190826074503-38ab9da00309
---
+++ pkg/specvalidate/multiClusterSpec.go
@@ -0,0 +1,259 @@
+package specvalidate
+
+var RuleSchema = `
+{
+	"$schema": "http://json-schema.org/draft-07/schema#",
+	"type": "object",
+	"properties": {
+	  "clusters": {
+		"type": "array",
+		"items": {
+		  "type": "object",
+		  "properties": {
+			"name": {
+			  "type": "string"
+			},
+			"type": { "enum": ["thanos", "grafana", "prometheus"] }
+		  },
+		  "required": ["name", "type"],
+		  "allOf": [
+			{
+			  "if": {
+				"properties": { "type": { "const": "thanos" } },
+				"required": ["type"]
+			  },
+			  "then": {
+				"properties": {
+				  "data": {
+					"type": "object",
+					"properties": {
+					  "name": {
+						"type": "string"
+					  },
+					  "querier": {
+						"type": "object",
+						"properties": {
+						  "name": {
+							"type": "string"
+						  },
+						  "targets": {
+							"type": "string"
+						  },
+						  "dedupEnbaled": {
+							"type": "string"
+						  },
+						  "autoDownSample": {
+							"type": "string"
+						  },
+						  "partialResponse": {
+							"type": "string"
+						  }
+						},
+						"required": ["name"]
+					  },
+					  "querierFE": {
+						"type": "object",
+						"properties": {
+						  "name": {
+							"type": "string"
+						  },
+						  "cacheOption": {
+							"mode": { "enum": ["inMemory", "memcached"] }
+						  },
+						  "memcachedOptions": {
+							"type": "object",
+							"properties": {
+							  "enabled": {
+								"type": "boolean"
+							  },
+							  "key": {
+								"type": "string"
+							  }
+							}
+						  }
+						},
+						"if": {
+						  "properties": {
+							"cacheOption": { "const": "memcached" }
+						  },
+						  "required": ["cacheOption"]
+						},
+						"then": {
+						  "required": ["memcachedOptions"]
+						},
+						"else": {
+						  "not": { "required": ["memcachedOptions"] }
+						},
+						"required": ["name", "cacheOption"]
+					  },
+					  "receiver": {
+						"type": "object",
+						"properties": {
+						  "name": {
+							"type": "string"
+						  }
+						},
+						"required": ["name"]
+					  },
+					  "compactor": {
+						"type": "object",
+						"properties": {
+						  "name": {
+							"type": "string"
+						  }
+						},
+						"required": ["name"]
+					  },
+					  "ruler": {
+						"type": "object",
+						"properties": {
+						  "alertManagers": {
+							"type": "array",
+							"items": [
+							  {
+								"type": "string"
+							  }
+							]
+						  },
+						  "config": {
+							"type": "string"
+						  }
+						},
+						"required": ["alertManagers", "config"]
+					  }
+					},
+					"additionalProperties": false,
+					"required": ["name", "querier"]
+				  }
+				}
+			  }
+			},
+			{
+			  "if": {
+				"properties": { "type": { "const": "grafana" } },
+				"required": ["type"]
+			  },
+			  "then": {
+				"properties": {
+				  "data": {
+					"type": "object",
+					"properties": {
+					  "name": {
+						"type": "string"
+					  },
+					  "setup": {
+						"type": "object",
+						"properties": {
+						  "enabled": {
+							"type": "boolean"
+						  },
+						  "name": {
+							"type": "string"
+						  },
+						  "namespace": {
+							"type": "string"
+						  }
+						},
+						"required": ["name", "namespace"]
+					  }
+					},
+					"additionalProperties": false,
+					"required": ["name", "setup"]
+				  }
+				}
+			  }
+			},
+			{
+			  "if": {
+				"properties": { "type": { "const": "prometheus" } },
+				"required": ["type"]
+			  },
+			  "then": {
+				"properties": {
+				  "data": {
+					"type": "object",
+					"properties": {
+					  "name": {
+						"type": "string"
+					  },
+					  "install": {
+						"type": "boolean"
+					  },
+					  "namespace": {
+						"type": "string"
+					  },
+					  "mode": { "enum": ["sidecar", "receiver"] },
+					  "receiveReference": {
+						"type": "string"
+					  },
+					  "objStoreConfig": {
+						"type": "string"
+					  }
+					},
+					"additionalProperties": false,
+					"if": {
+					  "properties": { "mode": { "const": "receiver" } },
+					  "required": ["mode"]
+					},
+					"then": {
+					  "required": ["receiveReference"]
+					},
+					"else": {
+					  "not": { "required": ["receiveReference"] }
+					},
+					"required": ["name", "namespace", "mode", "objStoreConfig"]
+				  }
+				}
+			  }
+			}
+		  ]
+		}
+	  },
+	  "objStoreConfigslist": {
+		"type": "array",
+		"items": [
+		  {
+			"type": "object",
+			"properties": {
+			  "name": {
+				"type": "string"
+			  },
+			  "type": {
+				"type": "string"
+			  },
+			  "config": {
+				"type": "object",
+				"properties": {
+				  "bucket": {
+					"type": "string"
+				  },
+				  "endpoint": {
+					"type": "string"
+				  },
+				  "accessKey": {
+					"type": "string"
+				  },
+				  "secretKey": {
+					"type": "string"
+				  }
+				},
+				"required": ["bucket", "endpoint", "accessKey", "secretKey"]
+			  },
+			  "bucketweb": {
+				"type": "object",
+				"properties": {
+				  "enabled": {
+					"type": "boolean"
+				  }
+				}
+			  }
+			},
+			"required": ["name", "type", "config"]
+		  }
+		]
+	  }
+	},
+	"required": ["clusters", "objStoreConfigslist"],
+	"additionalProperties": false
+  }
+`
