64d23eb6974023e42091da5c41d3802fb17594f5
JESWINKNINAN <jeswinjkn@gmail.com>
2021-10-28 18:08:34 +0530

    fix: change cmd uninstall to destroy

--- cli/cmd/spec/rootCmd.go
+++ cli/cmd/spec/rootCmd.go
@@ -25,8 +25,8 @@ var applyCmd = &cobra.Command{
 }
 
 var uninstallSpecCmd = &cobra.Command{
-	Use:   "uninstall",
-	Short: "Deletes the entire stack across clusters",
+	Use:   "destory",
+	Short: "Destory the entire stack across clusters",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return manageApp(cmd)
 	},
