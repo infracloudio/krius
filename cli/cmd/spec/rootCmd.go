a2a73b54b9e6abe41ac0530bbf42b5456a1fe193
JESWIN K NINAN <jeswinjkn@gmail.com>
2021-10-28 18:34:53 +0530

    Fix: Change spec command uninstall to destroy
    * fix: change the filename to destory
    
    * fix: change case to destroy
    
    * fix: change cmd uninstall to destroy
    
    * fix: change uninstall to destroy in rootcmd

--- cli/cmd/spec/rootCmd.go
+++ cli/cmd/spec/rootCmd.go
@@ -25,8 +25,8 @@ var applyCmd = &cobra.Command{
 }
 
 var uninstallSpecCmd = &cobra.Command{
-	Use:   "uninstall",
-	Short: "Deletes the entire stack across clusters",
+	Use:   "destroy",
+	Short: "Destroy the entire stack across clusters",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return manageApp(cmd)
 	},
